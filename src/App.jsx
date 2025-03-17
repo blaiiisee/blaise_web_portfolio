59865016f9037f400e36ebbba72f1bca33f1a7a1
blaiiisee <ivanblaise02@gmail.com>
2025-03-18 00:14:12 +0800

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import LiquidFrame from "./components/img-flow.jsx";
+
+function App() {
+  return (
+    <div>
+      <h1>My Animated Liquid Frame</h1>
+      <LiquidFrame />
+    </div>
+  );
+}
+
+export default App
---
+++ src/components/img-flow.jsx
@@ -0,0 +1,7 @@
+
+
+export default function LiquidFrame() {
+  return (
+        <img src="../public/Display_Photo.jpg" alt="Display Photo" />
+  );
+}
